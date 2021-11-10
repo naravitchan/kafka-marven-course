d24e48bcc4d4bc8273014f6a3d1e98cefddc7690
naravich@enres.co <seagame@seagames-MacBook-Pro.local>
2021-11-10 16:01:51 +0700

    add Demo With keys

--- src/main/java/com/github/seagame/kafka/tutorial1/ProducerDemoKeys.java
+++ src/main/java/com/github/seagame/kafka/tutorial1/ProducerDemoKeys.java
@@ -6,10 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
-public class ProducerDemoWithCallback {
-    public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+public class ProducerDemoKeys {
+    public static void main(String[] args) throws ExecutionException,InterruptedException {
+        Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
         String bootstrapServers = "127.0.0.1:9092";
         System.out.println("Create Producer Properties");
         Properties properties = new Properties();
@@ -20,10 +21,22 @@ public class ProducerDemoWithCallback {
         System.out.println("Create The Producer");
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
+
         System.out.println("Send data");
         for (int i=0; i<10; i++) {
+            String topic = "first_topic";
+            String value = "hello world" + Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
+
+            logger.info("Key: "+ key);
+            // id_0 partition 1
+            // id 1 - 0
+            // id 2 - 2
+            // id 3 - 0
+
+
             // create Producer record
-            ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "helo_world" + Integer.toString(i));
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
 
             producer.send(record, new Callback() {
                 @Override
@@ -39,7 +52,7 @@ public class ProducerDemoWithCallback {
                         logger.error("Error while producing", e);
                     }
                 }
-            });
+            }).get();
         }
         producer.flush();
         producer.close();
